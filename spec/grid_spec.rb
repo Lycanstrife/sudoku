76dfc452e914ab864e543654f8ce37ca845b39b1
Matt Thomas <matt@platformprojects.org>
2014-03-13 13:37:27 +0000

    Refactored for more legible code

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,20 +1,16 @@
 describe Grid do
 
-	context "initialization" do
+let(:grid) { #init in here }
+  
+  context "solving sudoku" do
 
-	  let(:puzzle) { '015003002000100906270068430490002017501040380003905000900081040860070025037204600' 
-	  # it's an easy sudoku puzzle, row by row }
-	  let(:grid) { Grid.new(puzzle) }
+    it "can solve the puzzle" do
+      expect(grid.solved?).to be_false
+      grid.solve
+      expect(grid.solved?).to be_true
+      expect(grid.to_s).to eq('615493872348127956279568431496832517521746389783915264952681743864379125137254698')
+    end
 
-	  it 'should have 81 cells' do
-	  end
-
-	  it 'should have an unsolved first cell' do
-	  end
-
-	  it 'should have a solved second cell with value 1' do
-	  end
-
-	end
-	
+  end
+  
 end
